e355bf31812a6b7fbfac32f9a8857f88e9d6d919
Jonas Maier <1.jmaier.3@gmail.com>
2019-11-28 15:06:57 +0100

    removed dead allocator code

--- src/allocator.rs
+++ src/allocator.rs
@@ -36,15 +36,3 @@ pub fn init_heap(
     Ok(())
 }
 
-pub struct Dummy;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
-    }
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("oh no there is no implementation for dealloc")
-    }
-}
-
-
