3206380424c8dd45f15f0a989e851501890d5279
Jonas Maier <1.jmaier.3@gmail.com>
2020-05-15 15:22:36 +0200

    file system: add default implementation for most functions

--- fs/src/filesystem.rs
+++ fs/src/filesystem.rs
@@ -10,11 +10,11 @@ use crate::block::*;
 /// Basic File-System functions
 pub trait BaseFileSystem {
     /// reads the contents of a directory in the file system
-    fn read_dir(&self, path: Path) -> FsResult<Vec<Filename>>;
+    fn read_dir(&self, path: Path) -> FsResult<Vec<Filename>> { Err(FsError::AccessViolation) }
     /// checks wether a directory exists or not
-    fn exists_dir(&self, path: Path) -> FsResult<bool>;
+    fn exists_dir(&self, path: Path) -> FsResult<bool> { Err(FsError::AccessViolation) }
     /// check wether a file exists or not
-    fn exists_file(&self, path: Path) -> FsResult<bool>;
+    fn exists_file(&self, path: Path) -> FsResult<bool> { Err(FsError::AccessViolation) }
 }
 
 /// Functions for a file system that can be mounted
@@ -34,13 +34,13 @@ where Self: Sized {
 pub trait ReadFileSystem {
     /// progress how far a file has been read, used as a handle to repeatedly read from the same
     /// file
-    type ReadProgress: Send;
+    type ReadProgress: Send; 
     /// opens a file and returns a progress handle to that file
-    fn open_read(&self, path: Path) -> FsResult<Self::ReadProgress>;
+    fn open_read(&self, path: Path) -> FsResult<Self::ReadProgress> { Err(FsError::AccessViolation) }
     /// reads from a file using the progress handle
-    fn read(&self, progress: &mut Self::ReadProgress, buffer: &mut [u8]) -> FsResult<usize>;
+    fn read(&self, progress: &mut Self::ReadProgress, buffer: &mut [u8]) -> FsResult<usize> { Err(FsError::AccessViolation) }
     /// seeks forward in the file
-    fn seek(&self, progress: &mut Self::ReadProgress, seek: usize) -> FsResult<()>;
+    fn seek(&self, progress: &mut Self::ReadProgress, seek: usize) -> FsResult<()> { Err(FsError::AccessViolation) }
 }
 
 /// Functions for a file system that supports writing files
@@ -49,21 +49,21 @@ pub trait WriteFileSystem {
     /// file
     type WriteProgress: Send;
     /// opens a file and returns a write handle to that file
-    fn open_write(&mut self, path: Path) -> FsResult<Self::WriteProgress>;
+    fn open_write(&mut self, path: Path) -> FsResult<Self::WriteProgress> { Err(FsError::AccessViolation) }
     /// writes to a file and updates the progress
-    fn write(&mut self, progress: &mut Self::WriteProgress, buffer: &[u8]) -> FsResult<()>;
+    fn write(&mut self, progress: &mut Self::WriteProgress, buffer: &[u8]) -> FsResult<()> { Err(FsError::AccessViolation) }
 }
 
 /// Functions for a file system that supports managing directories and file creation
 pub trait ManageFileSystem {
     /// deletes a file or directory
-    fn delete(&mut self, path: Path) -> FsResult<()>;
+    fn delete(&mut self, path: Path) -> FsResult<()> { Err(FsError::AccessViolation) }
     /// clears a file or directory
-    fn clear(&mut self, path: Path) -> FsResult<()>;
+    fn clear(&mut self, path: Path) -> FsResult<()> { Err(FsError::AccessViolation) }
     /// creates a new, empty file
-    fn create_file(&mut self,  path: Path) -> FsResult<()>;
+    fn create_file(&mut self,  path: Path) -> FsResult<()> { Err(FsError::AccessViolation) }
     /// creates a new, empty directory
-    fn create_dir(&mut self,  path: Path) -> FsResult<()>;
+    fn create_dir(&mut self,  path: Path) -> FsResult<()> { Err(FsError::AccessViolation) }
 }
 
 pub trait FunctionalFileSystem : BaseFileSystem + ReadFileSystem + WriteFileSystem + ManageFileSystem {}
